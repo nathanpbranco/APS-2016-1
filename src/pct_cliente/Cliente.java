4c228d881cdf0a4a26460f00801dd4cddf6b227c
Nathan P. Branco <nathan_pbranco@hotmail.com>
2016-05-10 17:28:26 -0300

    Adicionado o menu para ambas aplicações Servidor/Cliente

--- src/pct_cliente/Cliente.java
+++ src/pct_cliente/Cliente.java
@@ -11,11 +11,27 @@ import java.util.Scanner;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Cliente extends JFrame {
 
+	/*Sessão de objetos globais*/
+	
 	private JPanel contentPane;
-
+	private JTextField tfChat;
+	
+	static Socket cliente;
+	static PrintStream saída;
+	static Scanner leitor;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -34,15 +50,20 @@ public class Cliente extends JFrame {
 		/*Sessão do console*/
 		try {
 			/*Conecta com o servidor*/
-			Socket cliente = new Socket("192.168.25.194", 5000);
+			cliente = new Socket("192.168.25.194", 5001);
 			System.out.println("Conectado ao servidor!");
 			
 			/*Lê e envia os dados do cliente para o servidor*/
-			PrintStream saída = new PrintStream(cliente.getOutputStream());
-			Scanner leitor = new Scanner(System.in);
+			saída = new PrintStream(cliente.getOutputStream());
+			leitor = new Scanner(System.in);
 			while (leitor.hasNextLine()) {
 				saída.println(leitor.nextLine());
 			}
+			
+			/*Termina a conexão*/
+			saída.close();
+			leitor.close();
+			cliente.close();
 			
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
@@ -57,11 +78,50 @@ public class Cliente extends JFrame {
 	public Cliente() {
 		setTitle("Cliente");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 542, 359);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnArquivo = new JMenu("Arquivo");
+		menuBar.add(mnArquivo);
+		
+		JMenuItem mntmSair = new JMenuItem("Sair");
+		mnArquivo.add(mntmSair);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblStatus = new JLabel("Status:");
+		lblStatus.setBounds(10, 11, 184, 14);
+		contentPane.add(lblStatus);
+		
+		JTextArea taChat = new JTextArea();
+		taChat.setBounds(10, 36, 506, 241);
+		contentPane.add(taChat);
+		
+		JButton btnEnviar = new JButton("Enviar");
+		btnEnviar.setBounds(10, 287, 89, 23);
+		contentPane.add(btnEnviar);
+		
+		tfChat = new JTextField();
+		tfChat.setBounds(109, 288, 407, 20);
+		contentPane.add(tfChat);
+		tfChat.setColumns(10);
+		
+		/*Sessão de Action Handlers*/
+		
+		mntmSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					cliente.close();
+					saída.close();
+					leitor.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
-
 }
--- src/pct_servidor/Servidor.java
+++ src/pct_servidor/Servidor.java
@@ -16,15 +16,20 @@ import javax.swing.JButton;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Servidor extends javax.swing.JFrame {
+	
+	/*Sessão de objetos globais*/
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
+	private JTextField tfNome;
+	private JTextField tfIP;
+	
+	static ServerSocket servidor;
+	static Scanner leitor;
+	static Socket cliente;
 
 	/**
 	 * Launch the application.
@@ -42,37 +47,35 @@ public class Servidor extends javax.swing.JFrame {
 		});
 		
 		/*Sessão do console*/
+		
 		try {
 			/*Libera uma porta para que seja possivel uma conexão*/
-			ServerSocket servidor = new ServerSocket(5000);
+			servidor = new ServerSocket(5000);
 			System.out.println("A porta 5000 foi aberta...");
+			
 			/*A partir daqui, o processo é enterrompido pelo método accept().
 			 * O programa está esperando um usuario se conectar*/
-			Socket cliente = servidor.accept();
+			cliente = servidor.accept();
 			System.out.println("Nova conexão");
 			System.out.println("Nome: " + cliente.getInetAddress().getHostName());
 			System.out.println("IP: " + cliente.getInetAddress().getHostAddress());
 			
 			/*Recebe as informações enviadas pelo usuario*/
-			Scanner leitor = new Scanner(cliente.getInputStream());
+			leitor = new Scanner(cliente.getInputStream());
 			while (leitor.hasNextLine()) {
 				System.out.println(leitor.nextLine());
 			}
 			
-			/*Encerra as conexões*/
-			leitor.close();
-			servidor.close();
-			cliente.close();
-			
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
+			
 	}
 
 	public Servidor() {
 		setTitle("Servidor");
 		setDefaultCloseOperation(Servidor.EXIT_ON_CLOSE);
-		setBounds(100, 100, 644, 472);
+		setBounds(100, 100, 644, 428);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -88,57 +91,48 @@ public class Servidor extends javax.swing.JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNome = new JLabel("Nome:");
-		lblNome.setBounds(10, 11, 46, 14);
+		lblNome.setBounds(10, 35, 46, 14);
 		contentPane.add(lblNome);
 		
-		textField = new JTextField();
-		textField.setBounds(66, 8, 107, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		tfNome = new JTextField();
+		tfNome.setEditable(false);
+		tfNome.setBounds(66, 32, 107, 20);
+		contentPane.add(tfNome);
+		tfNome.setColumns(10);
 		
-		JLabel lblIp = new JLabel("IP:");
-		lblIp.setBounds(10, 36, 46, 14);
-		contentPane.add(lblIp);
+		JLabel lblIP = new JLabel("IP:");
+		lblIP.setBounds(10, 60, 46, 14);
+		contentPane.add(lblIP);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(66, 33, 107, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel lblNome_1 = new JLabel("Nome:");
-		lblNome_1.setBounds(459, 11, 46, 14);
-		contentPane.add(lblNome_1);
-		
-		textField_2 = new JTextField();
-		textField_2.setBounds(515, 8, 103, 20);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
-		
-		JLabel lblIp_1 = new JLabel("IP:");
-		lblIp_1.setBounds(459, 36, 46, 14);
-		contentPane.add(lblIp_1);
-		
-		textField_3 = new JTextField();
-		textField_3.setBounds(515, 33, 103, 20);
-		contentPane.add(textField_3);
-		textField_3.setColumns(10);
+		tfIP = new JTextField();
+		tfIP.setEditable(false);
+		tfIP.setBounds(66, 57, 107, 20);
+		contentPane.add(tfIP);
+		tfIP.setColumns(10);
 		
 		JLabel lblConectadoCom = new JLabel("Conectado com...");
-		lblConectadoCom.setBounds(258, 22, 113, 14);
+		lblConectadoCom.setBounds(10, 11, 113, 14);
 		contentPane.add(lblConectadoCom);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setLineWrap(true);
-		textArea.setBounds(10, 61, 608, 309);
-		contentPane.add(textArea);
+		JTextArea taChat = new JTextArea();
+		taChat.setLineWrap(true);
+		taChat.setBounds(10, 85, 608, 269);
+		contentPane.add(taChat);
 		
-		JButton btnEnviar = new JButton("Enviar");
-		btnEnviar.setBounds(10, 380, 77, 23);
-		contentPane.add(btnEnviar);
+		/*Sessão de Action Handlers*/
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(97, 381, 521, 20);
-		contentPane.add(textField_4);
-		textField_4.setColumns(10);
+		mntmSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				/*Encerra as conexões*/
+				try {
+					leitor.close();
+					servidor.close();
+					cliente.close();
+					System.exit(0);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
